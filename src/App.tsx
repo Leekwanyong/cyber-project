309b8e2a89c6a557eb236cb3ca5b98942ba10cab
Leekwanyong <dlrkdid12@gmail.com>
2025-02-22 02:51:28 +0900

    feat: 리셋css 최상위 컴포넌트 app에 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalStyles';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <Global styles={globalStyles} />
+    </div>
+  );
 }
 
 export default App;
