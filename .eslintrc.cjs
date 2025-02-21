459465f8ceca480b1abdbb34f738dab29eeaecda
Leekwanyong <dlrkdid12@gmail.com>
2025-02-22 00:58:44 +0900

    chore: react, eslint(airbnb), ts, prettier, 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['react',  'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        '@typescript-eslint/no-shadow': 'off',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
