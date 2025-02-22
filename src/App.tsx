9523825558a7cd84ff6b2acc060a170048dcfd6b
Leekwanyong <dlrkdid12@gmail.com>
2025-02-23 02:29:48 +0900

    feat: router 셋업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Global } from '@emotion/react';
+import { Route, Routes } from 'react-router-dom';
 import globalStyles from './styles/globalStyles';
+import Product from './pages/product/Product';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Global styles={globalStyles} />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </>
   );
 }
 
