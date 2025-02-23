38a7ac978a61e29d53a74ee816d6f011a2cd1279
Leekwanyong <dlrkdid12@gmail.com>
2025-02-23 18:03:40 +0900

    feat: query 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import globalStyles from './styles/globalStyles';
 import Product from './pages/product/Product';
 import Home from './pages/home/Home';
 
+const query = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={query}>
       <Global styles={globalStyles} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
