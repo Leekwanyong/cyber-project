a062a3f55c4378e745b891c1808bddfb005ea8e5
Leekwanyong <dlrkdid12@gmail.com>
2025-02-25 13:14:49 +0900

    feat: aside Menu, Data 다른 파일에 상수처리

---
+++ src/constants/aside.ts
@@ -0,0 +1,53 @@
+export const ASIDE_DATA = {
+  beauty: [
+    {
+      id: 1,
+      brand: 'Essence',
+      name: 'beauty',
+      checked: false,
+    },
+  ],
+
+  fragrance: [
+    {
+      id: 1,
+      brand: 'Calvin Klein',
+      name: 'fragrance',
+      checked: false,
+    },
+    {
+      id: 2,
+      brand: 'Chanel',
+      name: 'fragrance',
+      checked: false,
+    },
+    {
+      id: 3,
+      brand: 'Dior',
+      name: 'fragrance',
+      checked: false,
+    },
+    {
+      id: 4,
+      brand: 'Dolce & Gabbana',
+      name: 'fragrance',
+      checked: false,
+    },
+    {
+      id: 5,
+      brand: 'Gucci',
+      name: 'fragrance',
+      checked: false,
+    },
+  ],
+  furniture: [
+    {
+      id: 1,
+      brand: 'Annibale Colombo',
+      name: 'furniture',
+      checked: false,
+    },
+  ],
+};
+
+export const LABEL_MENU = ['beauty', 'fragrance', 'furniture'];
