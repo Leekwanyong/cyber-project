3331b1388509e90da18aa2b080ef0c7692dfbec0
Leekwanyong <dlrkdid12@gmail.com>
2025-02-27 17:43:05 +0900

    feat: wishlist store에 연결

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import asideReducer from './slice/asideSlice';
+import wishlistReducer from './slice/wishlistSlice';
 
 export const store = configureStore({
-  reducer: { asideSlice: asideReducer },
+  reducer: { asideSlice: asideReducer, wishlist: wishlistReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
