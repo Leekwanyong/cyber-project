038c89ef160d5a0885b56cfe3e02b7791ae7d2de
Leekwanyong <dlrkdid12@gmail.com>
2025-02-27 19:50:05 +0900

    fix: redux Provider 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
 import globalStyles from './styles/globalStyles';
 import Product from './pages/product/Product';
 import Home from './pages/home/Home';
 import StoredWishlist from './components/common/Cart/StoredWishlist';
+import { store } from './redux/store';
 
 const query = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={query}>
-      <Global styles={globalStyles} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/wishlist" element={<StoredWishlist />} />
-      </Routes>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={query}>
+        <Global styles={globalStyles} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/wishlist" element={<StoredWishlist />} />
+        </Routes>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
