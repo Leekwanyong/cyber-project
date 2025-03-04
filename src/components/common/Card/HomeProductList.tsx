de0cfb11e1d1e90673aaa46d3f4d63f87670218a
Leekwanyong <dlrkdid12@gmail.com>
2025-03-04 16:04:47 +0900

    refactor: 기존에 한 파일에 있던 것을 가독성과 의존성을 위해서 분리함

---
+++ src/components/common/Card/HomeProductList.tsx
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+import styled from '@emotion/styled';
+import useHomeProductList from '../../../hooks/useHomeProductList';
+import Skeleton from './Skeleton';
+import ProductItem from './ProductItem';
+import { HomeProductQuery } from '../../../types/card';
+
+const WrapperUl = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 10rem;
+  gap: 1rem;
+  padding: 0;
+`;
+
+function HomeProductList({ category, limit, isInfinity, isLastSection }: HomeProductQuery) {
+  const { isLoading, data } = useHomeProductList(category, limit, isInfinity);
+
+  return (
+    <WrapperUl style={{ display: 'flex' }}>
+      {isLoading
+        ? Array.from({ length: limit }).map(() => <Skeleton key={uuidv4()} />)
+        : data.map((product, index) => (
+            <ProductItem
+              key={product.id}
+              index={index}
+              item={product}
+              isLastSection={isLastSection}
+            />
+          ))}
+    </WrapperUl>
+  );
+}
+
+export default HomeProductList;
