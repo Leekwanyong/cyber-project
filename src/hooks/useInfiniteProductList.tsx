dd50ed4f55db62bd0e66c22d8537c4d21348f213
Leekwanyong <dlrkdid12@gmail.com>
2025-03-04 16:15:13 +0900

    refactor: useHook을 사용하여 무한스크롤 분리

---
+++ src/hooks/useInfiniteProductList.tsx
@@ -0,0 +1,23 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getStoreAllListApi } from '../api/getStoreApi';
+
+const UseInfiniteProductList = (isInfinity: boolean, limit: number) => {
+  const infiniteQuery = useInfiniteQuery({
+    queryKey: ['infinity'],
+    queryFn: ({ pageParam = 0 }) => getStoreAllListApi({ limit, pageParam }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length * limit : undefined;
+    },
+    enabled: isInfinity,
+  });
+  return {
+    isLoading: infiniteQuery.isLoading ?? false,
+    isFetchingNextPage: infiniteQuery.isLoading ?? false,
+    hasNextPage: infiniteQuery.hasNextPage ?? false,
+    fetchNextPage: infiniteQuery.fetchNextPage ?? (() => {}),
+    data: infiniteQuery.data?.pages.flat() ?? [],
+  };
+};
+
+export default UseInfiniteProductList;
