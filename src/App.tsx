7f897def371eb9411e2d63fbf6f083e0728dbaf0
Leekwanyong <dlrkdid12@gmail.com>
2025-04-04 00:55:36 +0900

    chore: 테일윈드 css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import Product from './pages/product/Product';
