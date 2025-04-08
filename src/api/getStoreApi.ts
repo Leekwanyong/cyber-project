54ca30ed0a54ed3425e1dfb9115f7068682cc9f6
Leekwanyong <dlrkdid12@gmail.com>
2025-04-08 21:41:19 +0900

    feat: 상세제품 페이지 api 구현

--- src/api/getStoreApi.ts
+++ src/api/getStoreApi.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Product } from '../types/card';
 
 interface GetStoreApi {
   category: string;
@@ -10,16 +11,24 @@ interface GetStoreAllListApi {
   pageParam: number;
 }
 
+const API = 'https://dummyjson.com/products';
+
 export const getStoreApi = async ({ category, limit }: GetStoreApi) => {
-  const response = await axios.get(
-    `https://dummyjson.com/products/category/${category}?limit=${limit}`
-  );
+  const response = await axios.get(`${API}/category/${category}?limit=${limit}`);
   return response.data.products;
 };
 
 export const getStoreAllListApi = async ({ limit, pageParam }: GetStoreAllListApi) => {
-  const response = await axios.get(
-    `https://dummyjson.com/products?limit=${limit}&skip=${pageParam}`
-  );
+  const response = await axios.get(`${API}?limit=${limit}&skip=${pageParam}`);
+  return response.data.products;
+};
+
+export const getStoreDetailApi = async (id: string | undefined): Promise<Product> => {
+  const response = await axios.get(`${API}/${id}`);
+  return response.data;
+};
+
+export const getStoreDetail = async (category: string | undefined) => {
+  const response = await axios.get(`${API}/category/${category}?limit=5`);
   return response.data.products;
 };
