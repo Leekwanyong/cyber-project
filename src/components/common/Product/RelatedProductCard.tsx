ebe3f53213208d812736454bcd8566e48765b32e
Leekwanyong <dlrkdid12@gmail.com>
2025-04-09 17:42:02 +0900

    feat: 이미지 WebP 최적화 및 에러 fallback 처리 추가

--- src/components/common/Product/RelatedProductCard.tsx
+++ src/components/common/Product/RelatedProductCard.tsx
@@ -1,17 +1,33 @@
 import { Product } from '../../../types/card';
+import UseOptimizedImage from '../../../hooks/useOptimizedImage';
+import imageFallbackHandler from '../../../utils/image';
+import DefaultImg from '../../../assets/defaultImg.png';
 
 interface RelatedProductCardProps {
   product: Product;
 }
 
 function RelatedProductCard({ product }: RelatedProductCardProps) {
+  const optimizedImage = UseOptimizedImage({
+    url: product.thumbnail || '',
+    isWebp: true,
+    width: 300,
+  });
   return (
     <article className="mt-12 mb-28 px-4 md:px-0 max-w-7xl mx-auto">
       <div
         key={product.id}
         className="bg-white shadow-md rounded-2xl overflow-hidden hover:scale-[1.02] transition-transform duration-300 cursor-pointer"
       >
-        <img src={product.thumbnail} alt={product.title} className="w-full h-30 object-cover" />
+        <picture>
+          <source type="image/webp" src={optimizedImage} />
+          <img
+            src={product.thumbnail}
+            alt={product.title}
+            className="w-full h-30 object-cover"
+            onError={imageFallbackHandler(DefaultImg, product.thumbnail || '')}
+          />
+        </picture>
         <div className="p-3">
           <h3 className="text-sm font-semibold truncate">{product.title}</h3>
           <p className="text-gray-500 text-xs mt-1">{product.brand}</p>
