24ed55ee4a37926be3c60a3742daa80097b878de
Leekwanyong <dlrkdid12@gmail.com>
2025-04-09 13:52:11 +0900

    feat: TextInput 컴포넌트에 onKeyDown 이벤트 핸들러 추가

--- src/components/common/Input/TextInput.tsx
+++ src/components/common/Input/TextInput.tsx
@@ -6,16 +6,23 @@ interface TextInputProps {
   size: TextInputSize;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-function TextInput({ size, onChange, value }: TextInputProps) {
+function TextInput({ size, onChange, value, onKeyDown }: TextInputProps) {
   const sizeStyle = {
     primary: 'w-full px-4 py-2 border outline-none rounded-md',
     medium: 'w-full',
   }[size];
 
   return (
-    <input value={value} className={`${sizeStyle}`} placeholder="Search" onChange={onChange} />
+    <input
+      value={value}
+      className={`${sizeStyle}`}
+      placeholder="Search"
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+    />
   );
 }
 
