bd96f6a01dcb82609c98cc0830c9264ad4f8e4fc
Leekwanyong <dlrkdid12@gmail.com>
2025-04-10 19:02:22 +0900

    design: RelatedProductCard css 수정

--- src/components/common/Product/RelatedProductCard.tsx
+++ src/components/common/Product/RelatedProductCard.tsx
@@ -14,29 +14,24 @@ function RelatedProductCard({ product }: RelatedProductCardProps) {
   });
 
   return (
-    <article className="mt-12 mb-28 px-4 md:px-0 max-w-7xl mx-auto">
-      <div
-        key={product.id}
-        className="bg-white shadow-md rounded-2xl overflow-hidden hover:scale-[1.02] transition-transform duration-300 cursor-pointer"
-      >
+    <article className="w-full max-w-[250px] flex flex-col bg-white shadow-md rounded-2xl overflow-hidden hover:scale-[1.02] transition-transform duration-300 cursor-pointer">
+      <div className="aspect-[1/1] w-full bg-gray-100">
         <picture>
           <source type="image/webp" src={optimizedImage} />
           <img
             src={product.thumbnail}
             alt={product.title}
-            className="w-full h-30 object-cover"
             width={300}
             height={300}
             onError={imageErrorHandler}
+            className="w-full h-full object-cover"
           />
         </picture>
-        <div className="p-3">
-          <h3 className="text-sm font-semibold truncate">{product.title}</h3>
-          <p className="text-gray-500 text-xs mt-1">{product.brand}</p>
-          <p className="text-blue-600 font-bold text-base mt-2">
-            ${product.price.toLocaleString()}
-          </p>
-        </div>
+      </div>
+      <div className="p-3 flex flex-col gap-1">
+        <h3 className="text-sm font-semibold truncate">{product.title}</h3>
+        <p className="text-gray-500 text-xs">{product.brand}</p>
+        <p className="text-blue-600 font-bold text-base mt-1">${product.price.toLocaleString()}</p>
       </div>
     </article>
   );
