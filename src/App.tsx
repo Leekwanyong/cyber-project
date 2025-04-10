2077ddd1b7bd65f815087d2351b817e82f3dbdd8
Leekwanyong <dlrkdid12@gmail.com>
2025-04-10 13:31:25 +0900

    feat: 라우터 lazy suspense 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
-import Product from './pages/product/Product';
+import { lazy, Suspense } from 'react';
 import Home from './pages/home/Home';
-import StoredWishlist from './components/common/Cart/StoredWishlist';
 import { store } from './redux/store';
 import Layout from './components/common/Layouts/Layout';
-import ProductDetailInfo from './components/common/Product/ProductDetailInfo';
+
+const Product = lazy(() => import('./pages/product/Product'));
+const StoredWishlist = lazy(() => import('./components/common/Cart/StoredWishlist'));
+const ProductDetailInfo = lazy(() => import('./components/common/Product/ProductDetailInfo'));
 
 const query = new QueryClient();
 
@@ -15,14 +17,16 @@ function App() {
   return (
     <Provider store={store}>
       <QueryClientProvider client={query}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/wishlist" element={<StoredWishlist />} />
-            <Route path="/product/:id" element={<ProductDetailInfo />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/wishlist" element={<StoredWishlist />} />
+              <Route path="/product/:id" element={<ProductDetailInfo />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </QueryClientProvider>
     </Provider>
   );
